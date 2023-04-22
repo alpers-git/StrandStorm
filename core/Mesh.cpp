ee320aa09d1c49f4a617176a3c94da21fe596dab
Amarnath <amarnath2105@gmail.com>
2023-04-21 23:12:15 -0600

    Changed init scale

--- core/Mesh.cpp
+++ core/Mesh.cpp
@@ -47,7 +47,7 @@ void HairMesh::loadFromFile(const std::string &modelPath, bool compNormals)
 
     // normalize the mesh to fit in a 5x5x5 cube
     mesh.ComputeBoundingBox();
-    const auto scale =  5.0f / (mesh.GetBoundMax() - mesh.GetBoundMin()).Length();
+    const auto scale =  4.0f / (mesh.GetBoundMax() - mesh.GetBoundMin()).Length();
     //go over the vertices and scale them
     for (int i = 0; i < mesh.NV(); i++) {
         mesh.V(i) *= scale;
@@ -155,7 +155,7 @@ void SurfaceMesh::loadFromFile(const std::string &modelPath, bool compNormals)
 
     // normalize the mesh to fit in a 5x5x5 cube
     mesh.ComputeBoundingBox();
-    const auto scale =  5.0f / (mesh.GetBoundMax() - mesh.GetBoundMin()).Length();
+    const auto scale =  4.0f / (mesh.GetBoundMax() - mesh.GetBoundMin()).Length();
     //go over the vertices and scale them
     for (int i = 0; i < mesh.NV(); i++) {
         mesh.V(i) *= scale;
